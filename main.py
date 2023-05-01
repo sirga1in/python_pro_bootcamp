3d7f624cf3bc9bebcd98d4734d2072b294b2722c
sirga1in <115899482+sirga1in@users.noreply.github.com>
2023-05-01 11:09:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
 #Write your code below this line 👇
-name = input("Write you name.\n")
-print('Your name length is ' + str(len(name)))
+
+a,b = b,a
 
 
+#Write your code above this line 👆
+####################################
 
-
-
-
-
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
 
 
